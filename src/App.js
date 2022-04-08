3d3ae190e688359b8fd89dc212157193a0e7d030
solo-mizan <mizanbd.eee@gmail.com>
2022-04-08 23:48:18 +0600

    react-bootstrap framework added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import { getAuth } from "firebase/auth";
+import { Button, Form } from "react-bootstrap";
 import './App.css';
 import app from './firebase.init';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const auth = getAuth(app);
 
 function App() {
+
+  const handleEmail = (event) => {
+    console.log(event.target.value);
+  }
+
+  const handleFormSubmit = (event) => {
+    console.log('form submitted');
+    event.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <form>
-        <input placeholder="Your email" type="text" name="" id="" />
-        <br />
-        <input placeholder="Your password" type="password" name="" id="" />
-      </form>
+    <div>
+      <div className="registration-form w-50 mx-auto border">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
